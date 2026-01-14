78f019440f1e4b5e567277eed6b63606aef7921a
Prithoo Pinak Medhi <prithoo11335@gmail.com>
2026-01-15 02:30:55 +0530

    new endpoints and APIs for signup and login.

---
+++ src/auth_app/apis.py
@@ -0,0 +1,42 @@
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+
+from auth_app.helpers import UserModelHelpers
+
+from auth_app import logger
+
+
+class UserRegisterAPI(APIView):
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request: Request) -> Response:
+        resp = UserModelHelpers.register(data=request.data)
+        return resp.to_response()
+    
+class AdminUserRegisterAPI(APIView):
+
+    permission_classes = (IsAdminUser,)
+
+    def post(self, request: Request) -> Response:
+        resp = UserModelHelpers.register(data=request.data, is_admin=True, is_staff=True)
+        return resp.to_response()
+    
+
+class UserPasswordLoginAPI(APIView):
+
+    permission_classes = (AllowAny,)
+
+    def post(self, request: Request) -> Response:
+        username = request.data.get("username")
+        email = request.data.get("email")
+        password = request.data.get("password")
+        resp = UserModelHelpers.login_via_password(
+            username=username,
+            email=email,
+            password=password
+        )
+        return resp.to_response()
---
+++ src/auth_app/endpoints.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from auth_app.apis import UserRegisterAPI, AdminUserRegisterAPI, UserPasswordLoginAPI
+
+
+PREFIX = "api/auth/"
+
+urlpatterns = [
+    path('register-normal/', UserRegisterAPI.as_view(), name='new-user-registration'),
+    path('register-sudo/', AdminUserRegisterAPI.as_view(), name='admin-user-registration'),
+    path('login/', UserRegisterAPI.as_view(), name='password-login'),
+]
